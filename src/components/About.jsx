8adeedf289a1ca15c5be9149c00771be6c636089
Singaravelan <singaravelan1228@gmail.com>
2026-04-06 15:23:40 +0530

    new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
       <div className="container slide-in">
         <h2 style={{ fontSize: '1.875rem', fontWeight: 'bold', marginBottom: '1rem' }}>About Me</h2>
         <p style={{ fontSize: '1.125rem', marginBottom: '1rem' }}>
-          “I'm Singaravelan D, a passionate React and Python developer with a BE in Computer Science & Engineering (CGPA 7.6, 2022–2025) from PSN College of Engineering and Technology. My journey in tech began with a diploma from IRT Polytechnic College, and I further strengthened my frontend skills through a UI Development course at WingsCAD, where I learned HTML, CSS, JavaScript, and React.”
+          “I'm Singaravelan D, a passionate React and Python (Django)developer with a BE in Computer Science & Engineering (CGPA 7.6, 2022–2025) from PSN College of Engineering and Technology. My journey in tech began with a diploma from IRT Polytechnic College, and I further strengthened my frontend skills through a Python Development course at WingsCAD, where I learned Python, Django and MySQL.”
         </p>
         <p style={{ fontSize: '1.125rem' }}>
           I specialize in building dynamic, responsive web applications using React and Django, with a focus on user-friendly interfaces and efficient backend solutions. I’m a quick learner, adaptable, and always eager to tackle new challenges in the IT sector with strong communication.
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 
 function Skills() {
   const skills = [
-    { name: 'React', level: 'Router, Hooks, Vite etc...' },
+    { name: 'React', level: 'Router, Hooks,REST API Vite etc...' },
     { name: 'Python', level: 'Oops, Lambda...' },
-    { name: 'Django', level: 'MVT, ORM, Admin dashboard etc...' },
+    { name: 'Django', level: 'MVT, ORM,DRF and Admin dashboard etc...' },
     { name: 'JavaScript', level: 'Advanced like Anonymous functions, Promises, Async/Await, etc...' },
     { name: 'HTML5 & CSS', level: 'Advanced' },
-    { name: 'MySQL', level: 'DDL, DQL, DML, DCL' },
+    { name: 'MySQL', level: 'DDL, DQL, DML, DCL and more...' },
     { name: 'Git & GitHub', level: 'Intermediate like push, pull, track, and get the code' },
   ];
 
